e71892b211f26035d713dcf2336a4138ec797dba
Nikita Moshkov <nikitam851@gmail.com>
2021-06-01 14:27:42 +0200

    Morgan fingerprints similarity script + reorganize

---
+++ python/fingerprint_similarity.py
@@ -0,0 +1,51 @@
+import csv
+import numpy as np
+from tqdm import tqdm
+from rdkit import Chem
+from rdkit import DataStructs
+from chemprop.data.utils import get_data_from_smiles
+from chemprop.features.features_generators import morgan_binary_features_generator
+
+### Paths to assay matrices here
+assay_train_file = 'assay_matrix_discrete_train_scaff_filt.csv'
+assay_test_file = 'assay_matrix_discrete_test_scaff.csv'
+###
+
+
+smiles = []
+smiles_ = []
+with open(assay_train_file) as f:
+    next(f)
+    reader = csv.reader(f)
+    for row in reader:
+        smiles.append([row[0]])
+        smiles_.append(row[0])
+        
+with open(assay_test_file) as f:
+    next(f)
+    reader = csv.reader(f)
+    for row in reader:
+        smiles.append([row[0]])
+        smiles_.append(row[0])
+            
+fingerprints = []
+data = get_data_from_smiles(smiles)
+data = data.mols(flatten=True)
+for i in range(len(data)):
+    mf = morgan_binary_features_generator(data[i])
+    fingerprints.append(mf)
+    
+fingerprints = np.array(fingerprints)
+
+#print(fingerprints.shape)
+np.savez('fingerprints.npz', features = fingerprints)
+
+fps = []
+for i in range(len(data)):
+    fps.append(Chem.RDKFingerprint(data[i]))
+
+similarity = np.zeros([16978, 16978])
+for (x, y), _ in tqdm(np.ndenumerate(similarity)):
+    similarity[x, y] = DataStructs.FingerprintSimilarity(fps[x], fps[y])
+    
+np.savez('similarity_fingerprints.npz', features = similarity)
