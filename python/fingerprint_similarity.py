ca0d91b50af48cdc92f6e6ea84ce52a3ba2e2443
Nikita <nikitam851@gmail.com>
2023-03-16 10:03:39 -0400

    Naming and paths changes

--- python/fingerprint_similarity.py
+++ python/fingerprint_similarity.py
@@ -6,46 +6,37 @@ from rdkit import DataStructs
 from chemprop.data.utils import get_data_from_smiles
 from chemprop.features.features_generators import morgan_binary_features_generator
 
-### Paths to assay matrices here
-assay_train_file = 'assay_matrix_discrete_train_scaff_filt.csv'
-assay_test_file = 'assay_matrix_discrete_test_scaff.csv'
-###
+# Generate Morgan fingerprints and get compound similarity matrix, 
+# the output of this script is available on Zenodo (see dataset link in the ReadMe).
 
+if __name__ == "__main__":
+    smiles_list_file = '../assay_data/smiles.txt'
 
-smiles = []
-smiles_ = []
-with open(assay_train_file) as f:
-    next(f)
-    reader = csv.reader(f)
-    for row in reader:
-        smiles.append([row[0]])
-        smiles_.append(row[0])
+    smiles = []
+    smiles_ = []
+    with open(smiles_list_file) as f:
+        next(f)
+        reader = csv.reader(f)
+        for row in reader:
+            smiles.append([row[0]])
+            smiles_.append(row[0])
+                
+    fingerprints = []
+    data = get_data_from_smiles(smiles)
+    data = data.mols(flatten=True)
+    for i in range(len(data)):
+        mf = morgan_binary_features_generator(data[i])
+        fingerprints.append(mf)
         
-with open(assay_test_file) as f:
-    next(f)
-    reader = csv.reader(f)
-    for row in reader:
-        smiles.append([row[0]])
-        smiles_.append(row[0])
-            
-fingerprints = []
-data = get_data_from_smiles(smiles)
-data = data.mols(flatten=True)
-for i in range(len(data)):
-    mf = morgan_binary_features_generator(data[i])
-    fingerprints.append(mf)
-    
-fingerprints = np.array(fingerprints)
+    fingerprints = np.array(fingerprints)
+    np.savez('fingerprints.npz', features = fingerprints)
 
-#print(fingerprints.shape)
-np.savez('fingerprints.npz', features = fingerprints)
+    fps = []
+    for i in range(len(data)):
+        fps.append(Chem.RDKFingerprint(data[i]))
 
-fps = []
-for i in range(len(data)):
-    fps.append(Chem.RDKFingerprint(data[i]))
-
-similarity = np.zeros([16978, 16978])
-for (x, y), _ in tqdm(np.ndenumerate(similarity)):
-    similarity[x, y] = DataStructs.FingerprintSimilarity(fps[x], fps[y])
-    
-np.savez('similarity_fingerprints.npz', features = similarity)
+    similarity = np.zeros([16978, 16978])
+    for (x, y), _ in tqdm(np.ndenumerate(similarity)):
+        similarity[x, y] = DataStructs.FingerprintSimilarity(fps[x], fps[y])
+        
+    np.savez('similarity_fingerprints.npz', features = similarity)
--- python/mean_and_median.py
+++ python/mean_and_median.py
@@ -2,91 +2,93 @@ import csv
 import numpy as np
 from decimal import *
 
-results = {}
-for i in range(5):
-	with open('../predictions/chemical_jan22_cv' + str(i) + '/2022_01_evaluation_all_data_python.csv', 'r') as f:
-		next(f)
-		reader = csv.reader(f)
-		for row in reader:
-			if row[2] not in results.keys():
-				results[row[2]] = {}
-			if row[0] not in results[row[2]].keys():
-				results[row[2]][row[0]] = []
 
-			if row[1] != 'NA':
-				results[row[2]][row[0]].append(float(Decimal(row[1])))
+if __name__ == "__main__":
+	results = {}
+	for i in range(5):
+		with open('../predictions/chemical_cv{}/2022_01_evaluation_all_data.csv'.format(i), 'r') as f:
+			next(f)
+			reader = csv.reader(f)
+			for row in reader:
+				if row[2] not in results.keys():
+					results[row[2]] = {}
+				if row[0] not in results[row[2]].keys():
+					results[row[2]][row[0]] = []
 
-
-header = ['assay_id','auc','descriptor', 'auc_50', 'auc_70','auc_90']
-with open('../predictions/scaffold_mean_python_jan22.csv', 'w') as f:
-	writer = csv.writer(f, lineterminator = '\n')
-	writer.writerow(header)
-	for modal in results.keys():
-		auc_50 = 0
-		auc_70 = 0
-		auc_90 = 0
-		for assay in results[modal].keys():
-
-			row = [assay]
-			mean_auc = np.mean( results[modal][assay])
-			row.append(float(Decimal(str(mean_auc)[:16])))
-			row.append(modal)
-			if float(Decimal(str(mean_auc)[:16])) > 0.5:
-				auc_50 = auc_50 + 1
-				row.append('TRUE')
-			else:
-				row.append('FALSE')
-			if float(Decimal(str(mean_auc)[:16])) > 0.7:
-				auc_70 = auc_70 + 1
-				row.append('TRUE')
-			else:
-				row.append('FALSE')
-			
-			if float(Decimal(str(mean_auc)[:16])) > 0.9:
-				auc_90 = auc_90 + 1
-				row.append('TRUE')
-			else:
-				row.append('FALSE')
-
-			writer.writerow(row)
-
-		print('50', modal, auc_50)		
-		print('70', modal, auc_70)
-		print('90', modal, auc_90)
+				if row[1] != 'NA':
+					results[row[2]][row[0]].append(float(Decimal(row[1])))
 
 
-print('median')
-with open('../predictions/scaffold_median_python_jan22.csv', 'w') as f:
-	writer = csv.writer(f, lineterminator = '\n')
-	writer.writerow(header)
-	for modal in results.keys():
-		auc_50 = 0
-		auc_70 = 0
-		auc_90 = 0
-		for assay in results[modal].keys():
-			row = [assay]
-			median_auc = np.median( results[modal][assay])
-			row.append(float(Decimal(str(median_auc)[:16])))
-			row.append(modal)
-			if float(Decimal(str(median_auc)[:16])) > 0.5:
-				auc_50 = auc_50 + 1
-				row.append('TRUE')
-			else:
-				row.append('FALSE')
-			if float(Decimal(str(median_auc)[:16])) > 0.7:
-				auc_70 = auc_70 + 1
-				row.append('TRUE')
-			else:
-				row.append('FALSE')
-			
-			if float(Decimal(str(median_auc)[:16])) > 0.9:
-				auc_90 = auc_90 + 1
-				row.append('TRUE')
-			else:
-				row.append('FALSE')
+	header = ['assay_id','auc','descriptor', 'auc_50', 'auc_70','auc_90']
+	with open('../predictions/scaffold_mean_AUC.csv', 'w') as f:
+		writer = csv.writer(f, lineterminator = '\n')
+		writer.writerow(header)
+		for modal in results.keys():
+			auc_50 = 0
+			auc_70 = 0
+			auc_90 = 0
+			for assay in results[modal].keys():
 
-			writer.writerow(row)
+				row = [assay]
+				mean_auc = np.mean( results[modal][assay])
+				row.append(float(Decimal(str(mean_auc)[:16])))
+				row.append(modal)
+				if float(Decimal(str(mean_auc)[:16])) > 0.5:
+					auc_50 = auc_50 + 1
+					row.append('TRUE')
+				else:
+					row.append('FALSE')
+				if float(Decimal(str(mean_auc)[:16])) > 0.7:
+					auc_70 = auc_70 + 1
+					row.append('TRUE')
+				else:
+					row.append('FALSE')
+				
+				if float(Decimal(str(mean_auc)[:16])) > 0.9:
+					auc_90 = auc_90 + 1
+					row.append('TRUE')
+				else:
+					row.append('FALSE')
 
-		print('50', modal, auc_50)
-		print('70', modal, auc_70)
-		print('90', modal, auc_90)
+				writer.writerow(row)
+
+			print('50', modal, auc_50)		
+			print('70', modal, auc_70)
+			print('90', modal, auc_90)
+
+
+	print('median')
+	with open('../predictions/scaffold_median_AUC.csv', 'w') as f:
+		writer = csv.writer(f, lineterminator = '\n')
+		writer.writerow(header)
+		for modal in results.keys():
+			auc_50 = 0
+			auc_70 = 0
+			auc_90 = 0
+			for assay in results[modal].keys():
+				row = [assay]
+				median_auc = np.median( results[modal][assay])
+				row.append(float(Decimal(str(median_auc)[:16])))
+				row.append(modal)
+				if float(Decimal(str(median_auc)[:16])) > 0.5:
+					auc_50 = auc_50 + 1
+					row.append('TRUE')
+				else:
+					row.append('FALSE')
+				if float(Decimal(str(median_auc)[:16])) > 0.7:
+					auc_70 = auc_70 + 1
+					row.append('TRUE')
+				else:
+					row.append('FALSE')
+				
+				if float(Decimal(str(median_auc)[:16])) > 0.9:
+					auc_90 = auc_90 + 1
+					row.append('TRUE')
+				else:
+					row.append('FALSE')
+
+				writer.writerow(row)
+
+			print('50', modal, auc_50)
+			print('70', modal, auc_70)
+			print('90', modal, auc_90)
